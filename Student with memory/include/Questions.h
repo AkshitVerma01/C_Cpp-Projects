d916a2dfa2e0b57e9d3b320cfd33284d4977e33b
AkshitVerma01 <51888222+AkshitVerma01@users.noreply.github.com>
2020-11-04 13:47:05 -0500

    Memory allocation with student files and grades

---
+++ Student with memory/include/Questions.h
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "student.h"
+
+student** create_class_list(char *filename, int *sizePtr);
+int find(int idNo, student **list, int size);
+void input_grades(char *filename, student **list, int size);
+void compute_final_course_grades(student **list, int size);
+void output_final_course_grades(char *filename, student **list, int size);
+
+void withdraw(int idNo, student **list, int *sizePtr);
+void destroy_list(student **list, int *sizePtr);
